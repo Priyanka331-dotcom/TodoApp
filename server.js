e4361ffc3bafcabcac5ce5e042519cde4a31b09f
priyasingh26208-netizen <priyasingh26208@gmail.com>
2026-04-17 09:42:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend working!");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
+node server.js
+app.listen(5000)
